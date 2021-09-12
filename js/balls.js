057a6c74a842a57fddb888f3a907346ad22029ff
Raeymakers <70637440+Raeymakers@users.noreply.github.com>
2021-09-12 14:17:23 +0200

    Add files via upload

---
+++ js/balls.js
@@ -0,0 +1,42 @@
+var balls = [
+    {
+        condition: 'ball',
+        color: "red",
+        image: "img/red.png"
+    },
+    {
+        condition: 'ball',
+        color: "blue",
+        image: "img/blue.png"
+    },
+    {
+        condition: 'ball',
+        color: "yellow",
+        image: "img/yellow.png"
+    },
+    {
+        condition: 'ball',
+        color: "green",
+        image: "img/green.png"
+    },
+    {
+        condition: 'ball',
+        color: "pink",
+        image: "img/pink.png"
+    },
+    {
+        condition: 'ball',
+        color: "white",
+        image: "img/white.png"
+    },
+    {
+        condition: 'ball',
+        color: "black",
+        image: "img/black.png"
+    },
+    {
+        condition: 'ball',
+        color: "purple",
+        image: "img/purple.png"
+    }
+];
---
+++ js/pics.js
@@ -0,0 +1,42 @@
+var pics = [
+    {
+        condition: 'pic',
+        size: "small",
+        image: "img/comb.png"
+    },
+    {
+        condition: 'pic',
+        size: "small",
+        image: "img/pear.png"
+    },
+    {
+        condition: 'pic',
+        size: "small",
+        image: "img/key.png"
+    },
+    {
+        condition: 'pic',
+        size: "small",
+        image: "img/glass.png"
+    },
+    {
+        condition: 'pic',
+        size: "big",
+        image: "img/elephant.png"
+    },
+    {
+        condition: 'pic',
+        size: "big",
+        image: "img/house.png"
+    },
+    {
+        condition: 'pic',
+        size: "big",
+        image: "img/plane.png"
+    },
+    {
+        condition: 'pic',
+        size: "big",
+        image: "img/truck.png"
+    }
+];
